beab819643c1813774054b82d1216b51a20431aa
Jerwell Savella <81293539+JerwellSavella@users.noreply.github.com>
2022-03-15 16:43:30 +0800

    stashed some files and removed testing

---
+++ University/Simpleifelsestatement.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main () {
+   // local variable declaration:
+   char grade;
+    
+    cout << "Enter your grade here: ";
+    cin >> grade;
+ 
+   // check the boolean condition
+   if( grade >= 3 ) {
+      // if condition is true then print the following
+      cout << "Congrats! Your Passed the semester!" << endl;
+   } else {
+      // if condition is false then print the following
+      cout << "I'm sorry, Better luck next sem :(" << endl;
+   }
+
+ 
+   return 0;
+} 
